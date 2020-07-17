deacff6c104cd537222d57281cb7f1d8288e9be4
Barbara Vreede <b.vreede@gmail.com>
2020-07-17 14:31:54 +0200

    edit test function

--- R/hello.R
+++ R/hello.R
@@ -1,19 +1,11 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-hello <- function() {
-  print("Hello, Anke!")
+#' Hello function
+#'
+#' Here is a test function for our package.
+#' It greets the user.
+#' @param name The name you want to greet.
+#' @export
+hello <- function(name) {
+  greeting <- paste0("Hello, ", name, "!")
+  print(greeting)
 }
 
