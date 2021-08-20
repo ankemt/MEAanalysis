fc20eca9dd67a481887fe1653b8a4497a577eb73
Anke Tukker <a.m.tukker@uu.nl>
2021-08-20 16:36:22 -0400

    Created test script and in this test renamed baseline and expsoure columns

--- R/treatment_ratio.R
+++ R/treatment_ratio.R
@@ -1,5 +1,5 @@
+# TEST SECTION ------------------------------------------------------------
 library(MEAanalysis)
-
 # load files
 test_baseline <- parse_MEA_file("inst/extdata/baseline_testfile.csv")
 test_exposure <- parse_MEA_file("inst/extdata/exposure_testfile.csv")
@@ -8,17 +8,22 @@ WA_test_baseline <-test_baseline$`Well averages`
 WA_test_exposure <- test_baseline$`Well averages`
 
 # add baseline to each parameter of baseline
-renamed_baseline <- colnames(WA_test_baseline) %>%
-  paste("baseline", colnames(WA_test_baseline), sep = "_")
+renamed_baseline <- paste("Baseline", colnames(WA_test_baseline), sep = "_")
 
 # add exposure to each parameter of exposure
-# merge to files
+renamed_exposure <- paste("Exposure", colnames(WA_test_exposure), sep = "_")
+
+# merge two files
+
+
 # calculate treatment ratio
 
 
 # calculate treatment ratios well Averages
 # (Parameter exposure / Parameter Baseline) *100%
 
+
+# FUNCTION SECTION --------------------------------------------------------
 #' Calculate the treatment ratio between exposure and baseline of MEA output files
 #' (Currently incomplete)
 #'
