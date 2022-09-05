2c026c358a2bdae0c49518a816c3161a9250d829
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-09-05 17:50:49 +0200

    change variable name treatment to design

--- R/treatment_ratio.R
+++ R/treatment_ratio.R
@@ -3,18 +3,20 @@
 #'
 #' @param exposurepath Path to file with exposure data
 #' @param baselinepath Path to file with baseline data
-#' @param treatmentpath Path to file with treatment lay-out
+#' @param designpath Path to file with treatment lay-out
 #' @return file with [1] treatment ratio per parameter per well
 #' @export
-treatment_ratio <- function(exposurepath, baselinepath, treatmentpath){
+treatment_ratio <- function(exposurepath, baselinepath, designpath){
   exposure <- parse_MEA_file(exposurepath)
   baseline <- parse_MEA_file(baselinepath)
+  # design <- parse_designfile(designpath)
 
   # TODO calculate differences
   # TODO parse treatment information
   # write file
+
   # should this function create the file and return the path, or open the file?
 
-  return(resultfile)
+  return(exposure)
 }
 
