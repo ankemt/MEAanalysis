74ec537a4c6d8559becf5b0063dabe0c4f83f0fb
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-10-14 21:25:24 +0200

    parse design from designfile to tibble

--- R/parse_designfile.R
+++ R/parse_designfile.R
@@ -1,4 +1,4 @@
-#' Parse designfile
+#' Parse design file
 #'
 #' Parses a text file containing the design of the experiment.
 #'
@@ -14,8 +14,9 @@ parse_designfile <- function(path){
   metadata_names <- c("Date", "ExperimentID", "Total_wells")
 
   # make objects to fill
-  metadata <- NULL
   designlist <- NULL
+  metadata <- NULL
+  design <- NULL
 
   for(i in info){
     if(stringr::str_starts(i, "#")){next} # remove any lines that start with #
@@ -25,13 +26,17 @@ parse_designfile <- function(path){
     # get date, experimentID, total_wells
     if(i[1] %in% metadata_names){
       metadata <- rbind(metadata,i)
+    } else{
+      if(length(i) > 2){
+        design <- rbind(design, i)
+      }
     }
-
     # get groups
 
   }
 
-  metadata <- make_meta_df(metadata)
+  metadata <- rotate_to_df(metadata)
+  design <- make_design_df(design)
 
   designlist <- list("metadata" = metadata,
                      "design" = design)
@@ -39,13 +44,33 @@ parse_designfile <- function(path){
 }
 
 
-make_meta_df <- function(df){
+rotate_to_df <- function(df){
   df <- t(df)
   df <- as.data.frame(df)
   names(df) <- df[1,]
-  df <- df[2,]
+  df <- df[-1,]
   row.names(df) <- NULL # otherwise the row number is 2
   #TODO type of date and number is a character, fix this?
   return(df)
 }
+
+
+make_design_df <- function(df){
+  # ensure the first two columns are empty, before removing them
+  if(paste(df[,1],collapse="") != "" |
+     paste(df[,2],collapse="") != ""){
+    stop("Experimental design is unclear. Ensure that every group is preceded by two spaces.")
+  }
+  df <- df[,-c(1,2)]
+
+  df <- rotate_to_df(df)
+
+  # pivot to two columns and sort
+  df <- tidyr::pivot_longer(df, cols = everything(), names_to = "Group", values_to = "Well")
+  df <- dplyr::select(df, Well, Group)
+  df <- dplyr::arrange(df, Well)
+
+  return(df)
+}
+
 
