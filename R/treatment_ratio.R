09a49e7f8d39b0e98b587199d7ec43a10840ff4a
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-10-15 07:09:01 +0200

    join information from baseline and exposure files and calculate ratio

--- R/treatment_ratio.R
+++ R/treatment_ratio.R
@@ -4,19 +4,40 @@
 #' @param exposurepath Path to file with exposure data
 #' @param baselinepath Path to file with baseline data
 #' @param designpath Path to file with treatment lay-out
-#' @return file with [1] treatment ratio per parameter per well
+#' @return data frame with treatment ratio per parameter per well
 #' @export
 treatment_ratio <- function(exposurepath, baselinepath, designpath){
-  exposure <- parse_MEA_file(exposurepath)
   baseline <- parse_MEA_file(baselinepath)
-  # design <- parse_designfile(designpath)
+  exposure <- parse_MEA_file(exposurepath)
+  design <- parse_designfile(designpath)
 
-  # TODO calculate differences
-  # TODO parse treatment information
-  # write file
+  # collect all metadata
+  metadata <- list(
+    design = design$metadata,
+    exposure = exposure$Header,
+    baseline = baseline$Header
+  )
+  # TODO metadata is not passed outside the function
 
-  # should this function create the file and return the path, or open the file?
+  df_base <- tidyr::pivot_longer(baseline$`Well averages`,
+                                 cols = tidyr::contains("Metrics"),
+                                 names_to = "Parameter",
+                                 values_to = "Baseline_value")
+  df_expo <- tidyr::pivot_longer(exposure$`Well averages`,
+                                 cols = tidyr::contains("Metrics"),
+                                 names_to = "Parameter",
+                                 values_to = "Exposure_value")
+  df <- dplyr::full_join(df_base, df_expo, by = c("ID", "Parameter"))
 
-  return(exposure)
+  df <- dplyr::right_join(design$design, df, by = c("Well" = "ID"))
+
+  df <- dplyr::mutate(df,
+                      # TODO, this is not absolute v ratio, I think?
+                      Treatment_absolute = Exposure_value / Baseline_value,
+                      Treatment_ratio = Treatment_absolute * 100)
+
+  # TODO should this function create the file and return the path, or open the file?
+
+  return(df)
 }
 
