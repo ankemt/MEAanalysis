a300e160e1223285d3f4acd3799fbd1d6b39d8c5
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-10-21 22:10:25 +0200

    remove extra exposure testfile and corresp test

--- tests/testthat/test-treatment_ratio.R
+++ tests/testthat/test-treatment_ratio.R
@@ -17,14 +17,4 @@ test_that("treatment ratio file contains expected parameters", {
     testthat::expect_true(param%in%result$Parameter)
   }
 
-  # if the headers don't match, an error is produced
-  exposure_source_2 <- system.file("extdata", "exposure_testfile_2.csv", package = "MEAanalysis")
-  expect_error(
-    result <- treatment_ratio(
-      exposurepath = exposure_source_2,
-      baselinepath = baseline_source,
-      designpath = design_source),
-    regexp = "The MEA files provided have different metadata attributes"
-  )
-
 })
