eb147abd6e89930837c935682dbb083a0312f833
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-11-05 14:45:16 +0100

    fix intentionally failing test and add comment about platforms

--- tests/testthat/test-create_path.R
+++ tests/testthat/test-create_path.R
@@ -1,8 +1,11 @@
 test_that("folder path parsed correctly", {
-  # TODO this test will only work on unix because of separators.
-  # How do we fix this for windows?
-
+  # These tests currently work on all platforms (macOSX, ubuntu, windows; 20221105).
+  # However, the function create_path uses a platform-dependent separator,
+  # created with .Platform$file.sep
+  # This is a / on all platforms, apparently; otherwise tests would fail.
   resultpath <- "this/is/my/path"
+  winpath <- "C://dir/subdir"
+  rootpath <- "/this/is/my/path"
 
   testpath <- create_path("this/is/my/path")
   expect_equal(testpath, resultpath)
@@ -11,13 +14,11 @@ test_that("folder path parsed correctly", {
   expect_equal(testpath, resultpath)
 
   testpath <- create_path("this/is/my/path/")
-  expect_equal(testpath, "resultpath")
+  expect_equal(testpath, resultpath)
 
-  rootpath <- "/this/is/my/path"
   testpath <- create_path("/this/is/my/path/")
   expect_equal(testpath, rootpath)
 
-  winpath <- "C://dir/subdir"
   testpath <- create_path("C://dir/subdir")
   expect_equal(testpath, winpath)
   })
