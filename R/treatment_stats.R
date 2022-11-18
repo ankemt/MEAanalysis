502ecfe9dfafd3076cf86a524b2b2012b61695c5
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-11-18 21:57:29 +0100

    basic function that creates a grouped df

---
+++ R/treatment_stats.R
@@ -0,0 +1,14 @@
+#' Generate statistics for a treatment dataframe
+#'
+#' @param ratio_df resulting dataframe of the `treatment_ratio` function
+#'
+#' @return dataframe with summary statistics
+#' @export
+treatment_stats <- function(ratio_df){
+
+  df <- ratio_df |>
+    dplyr::group_by(Group, Metric_type, Parameter) |>
+    dplyr::summarize(Ratio_avg = mean(Treatment_ratio),
+                     Ratio_stdv = sd(Treatment_ratio))
+  return(df)
+}
